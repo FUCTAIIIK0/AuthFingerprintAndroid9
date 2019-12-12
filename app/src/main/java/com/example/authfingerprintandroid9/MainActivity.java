ea8b91be96b94bd30f854576cd7fe80c0f40e6b9
Ilya Desktop <ig@pss.one>
2019-12-12 15:56:58 +0300

    add fingerprintBtn

--- app/src/main/java/com/example/authfingerprintandroid9/MainActivity.java
+++ app/src/main/java/com/example/authfingerprintandroid9/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.authfingerprintandroid9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.main_fingerBtn)
+    Button fingerprintBtn;
+    
+    private final String TAG = "main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        
+        fingerprintBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Fingerprint");
+            }
+        });
     }
 }
